6e2d3f3ca85f4d6381a904326dd24f85744b3f9d
Joel Stockamp and sofia giordano <joel@>
2019-08-20 11:29:49 -0700

    add forEach loop, and branching

--- Desktop/puzzle/js/scripts.js
+++ Desktop/puzzle/js/scripts.js
@@ -1,19 +1,49 @@
-
-$(document).ready(function(){
-  $("form.phrases").submit(function(event){
+$(document).ready(function() {
+  $("form.phrases").submit(function(event) {
     event.preventDefault();
 
     var userPhrase = "If the grass is greener on the other side, you can bet the water bill is higher.";
-        console.log(userPhrase);
-
-    // userphrase = $(".ui1").val
-
-    // using a regex to search for vowels. no loop necessary. 
-    var regex = /[aeiou]/g
-    var newPhrase = userPhrase.replace(regex, "-");
-
     console.log(userPhrase);
-    console.log(newPhrase);
-    $("#result").text(newPhrase);
+
+    var characters = [];
+    characters = userPhrase.split("");
+
+
+    console.log(characters);
+
+    //needed triple equals to assign 
+    characters.forEach(function(character) {
+      if ((character.toLowerCase() === "a") || (character.toLowerCase() === "e") || (character.toLowerCase() === "i") || (character.toLowerCase() === "o") || (character.toLowerCase() === "u")) {
+      // if (character === "a") {
+        character = "-";
+        $("#result").append(character);
+      } else {
+        $("#result").append(character);
+      }
+    });
+
+
+
+
   });
 });
+
+
+// $(document).ready(function(){
+//   $("form.phrases").submit(function(event){
+//     event.preventDefault();
+//
+//     var userPhrase = "If the grass is greener on the other side, you can bet the water bill is higher.";
+//         console.log(userPhrase);
+//
+//     // userphrase = $(".ui1").val
+//
+//     // using a regex to search for vowels. no loop necessary.
+//     var regex = /[aeiou]/g
+//     var newPhrase = userPhrase.replace(regex, "-");
+//
+//     console.log(userPhrase);
+//     console.log(newPhrase);
+//     $("#result").text(newPhrase);
+//   });
+// });
